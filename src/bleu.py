a214df4ae57fb7e3ef725c5b6ae61af035fce94b
Adam <acohn74@gmail.com>
2020-08-21 20:12:45 +0300

    Vectorize score calculations in run method (using apply(lambda x:)). Add type hinting to class methods

--- src/bleu.py
+++ src/bleu.py
@@ -14,7 +14,7 @@ class Bleu:
         nltk.download('averaged_perceptron_tagger')
         nltk.download('stopwords')
 
-    def preprocess(self, raw_text, stopwords_remove=True):
+    def preprocess(self, raw_text: str, stopwords_remove=True) -> list:
         # keep only words
         letters_only_text = re.sub("[^a-zA-Z]", " ", str(raw_text))
         # convert to lower case and split 
@@ -25,25 +25,27 @@ class Bleu:
         cleaned_words = list(set([w for w in words if w not in stopword_set]))
         return cleaned_words if stopwords_remove else words
 
-    def BLEU1score(self,s1, s2,stopwords_remove=True):
-        hypothesis = [word for word in self.preprocess(s1,stopwords_remove)]
-        reference = [word for word in self.preprocess(s2,stopwords_remove)]
+    def BLEU1score(self, s1: str, s2: str, stopwords_remove=True) -> float:
+        hypothesis = [word for word in self.preprocess(s1, stopwords_remove)]
+        reference = [word for word in self.preprocess(s2, stopwords_remove)]
         #print(hypothesis,",",reference)
         BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis, weights=(1, 0, 0, 0))
         return BLEUscore
 
+    def run(self, df: pandas.DataFrame) -> pandas.DataFrame:
 
-    def run(self, df):
-
-        df['bleu_allwords'] = 0
-        df['bleu_withoutstop'] = 0
-
-        for i in range(df.shape[0]):
-            s1 = str(df['text_1'][i])
-            s2 = str(df['text_2'][i])
-            #print(s1,",",s2)
-            df.loc[i, 'bleu_allwords'] = self.BLEU1score(s1, s2, stopwords_remove=False)
-            df.loc[i, 'bleu_withoutstop'] = self.BLEU1score(s1, s2, stopwords_remove=True)
+        df['bleu_allwords'] = df.apply(lambda x: self.BLEU1score(str(x.text_1), str(x.text_2),
+                                                                 stopwords_remove=False), axis=1)
+        df['bleu_withoutstop'] = df.apply(lambda x: self.BLEU1score(str(x.text_1), str(x.text_2),
+                                                                    stopwords_remove=False), axis=1)
+        # df['bleu_allwords'] = 0
+        # df['bleu_withoutstop'] = 0
+        # for i in range(df.shape[0]):
+        #     s1 = str(df['text_1'][i])
+        #     s2 = str(df['text_2'][i])
+        #     #print(s1,",",s2)
+        #     df.loc[i, 'bleu_allwords'] = self.BLEU1score(s1, s2, stopwords_remove=False)
+        #     df.loc[i, 'bleu_withoutstop'] = self.BLEU1score(s1, s2, stopwords_remove=True)
 
         return df
 
