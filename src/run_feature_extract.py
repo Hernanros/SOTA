1138e40aa1dd4f6c73185ae40204abccc225ce96
Adam <acohn74@gmail.com>
2020-08-25 13:06:06 +0300

    Including WMD, POS Distance, & Ngram Overlap metrics, add max_n argument for instantiating NgramOverlap class

--- src/run_feature_extract.py
+++ src/run_feature_extract.py
@@ -1,3 +1,7 @@
+"""
+This file implements running the different semantic similiarity metrics on a dataset of paired sentences
+"""
+
 import pickle
 import argparse
 from .bleu import Bleu
@@ -5,6 +9,9 @@ from .cosine_similarites import CosineSimilarity
 from .elmo_euclidean_distance import EuclideanElmoDistance
 from .bertscore import BertScore
 from .chrFScore import chrFScore
+from .WMD import WMD
+from .POS_distance import POSDistance
+from .ngram_overlap import NgramOverlap
 
 
 def main(args):
@@ -12,7 +19,8 @@ def main(args):
 
     picklefile = 'data\combined_data_test.pickle'
     extractors = dict(bleu=Bleu(), cosine_similarites=CosineSimilarity(), elmo_similarites=EuclideanElmoDistance(),
-                      bert=BertScore(), chrf_score=chrFScore())
+                      bert=BertScore(), chrf_score=chrFScore(), pos_distance=POSDistance(), wmd=WMD(),
+                      ngram_overlap=NgramOverlap(args.max_n))
     features = args.features
     if features == 'ALL':
         features = list(extractors.keys())
@@ -34,6 +42,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pickle', type=str, help='pickle path for combined dataset')
     parser.add_argument('--features', type=str, help='use "ALL" for all features, or comma separated list of features')
+    parser.add_argument('--max_n', type=int, help='maximum number of n-gram overlap score to calculate, e.g. max_n=2 creates 1-gram-overlap & 2-gram-overlap')
 
     args = parser.parse_args()
     main(args)
