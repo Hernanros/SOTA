e354e467f955714798aab0eaeddbe88f942218a2
Adam <acohn74@gmail.com>
2020-08-25 13:17:54 +0300

    Streamlining metric import statments

--- src/run_feature_extract.py
+++ src/run_feature_extract.py
@@ -4,15 +4,7 @@ This file implements running the different semantic similiarity metrics on a dat
 
 import pickle
 import argparse
-from .bleu import Bleu
-from .cosine_similarites import CosineSimilarity
-from .elmo_euclidean_distance import EuclideanElmoDistance
-from .bertscore import BertScore
-from .chrFScore import chrFScore
-from .WMD import WMD
-from .POS_distance import POSDistance
-from .ngram_overlap import NgramOverlap
-from .ROUGE import ROUGE
+from src import *
 
 
 def main(args):
