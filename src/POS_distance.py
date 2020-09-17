81bb1d5bba20efefc78e67ea8590fb6441c6525d
Adam <acohn74@gmail.com>
2020-09-17 10:57:29 +0300

    Align word overlap and POS distance

--- src/POS_distance.py
+++ src/POS_distance.py
@@ -1,5 +1,5 @@
 """implements POS distance metric as a class"""
-import pandas
+import pandas as pd
 from nltk import pos_tag, word_tokenize
 import nltk
 import torchtext.vocab as torch_vocab
@@ -12,56 +12,50 @@ class POSDistance:
         nltk.download('averaged_perceptron_tagger')
         self.dic_glove = torch_vocab.GloVe(name='twitter.27B', dim=100)
 
-    def run(self, df: pandas.DataFrame) -> pandas.DataFrame:
+    def pos_distance(self, row):
+        temp_res_ori = pos_tag(row.text_1)
+        temp_res_gen = pos_tag(row.text_2)
+        temp_nn_ori = []
+        temp_nn_gen = []
+        temp_nn_vector_ori = []
+        temp_nn_vector_gen = []
+        for tube in temp_res_ori:
+            if tube[1] == 'NN' or tube[1] == 'NNS':
+                temp_nn_ori.append(tube[0])
+        for tube in temp_res_gen:
+            if tube[1] == 'NN' or tube[1] == 'NNS':
+                temp_nn_gen.append(tube[0])
+        for word in temp_nn_ori:
+            try:
+                temp_nn_vector_ori.append(self.dic_glove.vectors[self.dic_glove.stoi[word]])
+            except KeyError:
+                a = 1
+        for word in temp_nn_gen:
+            try:
+                temp_nn_vector_gen.append(self.dic_glove.vectors[self.dic_glove.stoi[word]])
+            except KeyError:
+                a = 1
+        if temp_nn_vector_ori != [] and temp_nn_vector_gen != []:
+            loss_list = []
+            for vector_target in temp_nn_vector_ori:
+                for vector_gen in temp_nn_vector_gen:
+                    tensor_gen = torch.FloatTensor(vector_gen)
+                    tensor_target = torch.FloatTensor(vector_target)
+                    temp_loss = torch.dist(tensor_gen, tensor_target)
+                    loss_list.append(temp_loss)
+            loss_list_new = sorted(loss_list)
+            loss_list_new1 = loss_list_new[:min(len(temp_nn_vector_ori), len(temp_nn_vector_gen))]
+            loss = (sum(loss_list_new1) / len(loss_list_new1)) * (
+                    1 + abs(len(temp_nn_vector_ori) - len(temp_nn_vector_gen)) / len(temp_nn_vector_ori))
+            return loss.numpy().item()
+        else:
+            return -1
 
-        loss_nn_list = []
-        total_loss_nn = 0
-        count = 0
-        i = 0
-        while i < len(df):
-            sentence_ori = df.iloc[i]['text_1'].copy()
-            sentence_gen = df.iloc[i]['text_2'].copy()
-            temp_res_ori = pos_tag(word_tokenize(sentence_ori))
-            temp_res_gen = pos_tag(word_tokenize(sentence_gen))
-            temp_nn_ori = []
-            temp_nn_gen = []
-            temp_nn_vector_ori = []
-            temp_nn_vector_gen = []
-            for tube in temp_res_ori:
-                if tube[1] == 'NN' or tube[1] == 'NNS':
-                    temp_nn_ori.append(tube[0])
-            for tube in temp_res_gen:
-                if tube[1] == 'NN' or tube[1] == 'NNS':
-                    temp_nn_gen.append(tube[0])
-            for word in temp_nn_ori:
-                try:
-                    temp_nn_vector_ori.append(self.dic_glove.vectors[self.dic_glove.stoi[word]])
-                except KeyError:
-                    a = 1
-            for word in temp_nn_gen:
-                try:
-                    temp_nn_vector_gen.append(self.dic_glove.vectors[self.dic_glove.stoi[word]])
-                except KeyError:
-                    a = 1
-            if temp_nn_vector_ori != [] and temp_nn_vector_gen != []:
-                loss_list = []
-                for vector_target in temp_nn_vector_ori:
-                    for vector_gen in temp_nn_vector_gen:
-                        tensor_gen = torch.FloatTensor(vector_gen)
-                        tensor_target = torch.FloatTensor(vector_target)
-                        temp_loss = torch.dist(tensor_gen, tensor_target)
-                        loss_list.append(temp_loss)
-                loss_list_new = sorted(loss_list)
-                loss_list_new1 = loss_list_new[:min(len(temp_nn_vector_ori), len(temp_nn_vector_gen))]
-                loss = (sum(loss_list_new1) / len(loss_list_new1)) * (
-                            1 + abs(len(temp_nn_vector_ori) - len(temp_nn_vector_gen)) / len(temp_nn_vector_ori))
-                total_loss_nn += loss
-                loss_nn_list.append(loss.numpy().item())
-                count += 1
-            else:
-                loss_nn_list.append(-1)
-            i += 1
+    def run(self, df: pd.DataFrame) -> pd.DataFrame:
 
-        df['POS dist score'] = loss_nn_list
+        text1 = df['text_1'].str.strip().str.split()
+        text2 = df['text_2'].str.strip().str.split()
+        pairs = pd.concat([text1, text2], axis=1)
+        df['POS Dist score'] = pairs.apply(lambda row: self.pos_distance(row), axis=1)
         return df
 
--- src/ngram_overlap.py
+++ src/ngram_overlap.py
@@ -1,29 +1,21 @@
 import pandas as pd
-from nltk.tokenize import RegexpTokenizer
 import numpy as np
 
 
 class NgramOverlap:
 
     def __init__(self, n):
-        self.tokenizer = RegexpTokenizer(r'\w+')
         self.n = n
 
-    def ngrammer(self, sent, gram):
-        grams = []
-        tokenized = self.tokenizer.tokenize(sent)
-        if len(tokenized) >= gram:
-            for i in range(len(tokenized) - gram + 1):
-                grams.append(' '.join(tokenized[i:i + gram]))
-        return grams
-
-    def gram_overlap(self, sent_a, sent_b, gram):
-        grams_a = set(self.ngrammer(sent_a, gram))
-        grams_b = set(self.ngrammer(sent_b, gram))
-        intersection = np.sum([gram in grams_b for gram in grams_a])
-        return 2 * intersection / (len(grams_a) + len(grams_b))
+    def gram_overlap(self, sent_a, sent_b):
+        first_sentence_set = set(sent_a)
+        second_sentence_set = set(sent_b)
+        score_wo = len(first_sentence_set & second_sentence_set) / len(first_sentence_set | second_sentence_set)
+        return score_wo
 
     def run(self, df: pd.DataFrame) -> pd.DataFrame:
-        for n in range(self.n):
-            df[f'{n + 1}-gram_overlap'] = df.apply(lambda x: self.gram_overlap(x.text_1, x.text_2, n + 1))
+        text1 = df['text_1'].str.strip().str.split()
+        text2 = df['text_2'].str.strip().str.split()
+        pairs = pd.concat([text1, text2], axis=1)
+        df['1-gram_overlap'] = pairs.apply(lambda row: self.gram_overlap(row.text_1, row.text_2,))
         return df
