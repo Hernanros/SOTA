09b01512e805b891008c379fb93827ececf948e1
Adam <acohn74@gmail.com>
2020-09-25 13:34:37 +0300

    Align chrFScore

--- src/features/chrFScore.py
+++ src/features/chrFScore.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from nltk.translate import chrf_score
+from src.features import Metric
 
+class chrFScore(Metric):
 
-class chrFScore:
-
-    def __init__(self):
-        pass
+    def __init__(self, val):
+        super(chrFScore, self).__init__(val=val)
 
     def run(self, df: pd.DataFrame) -> pd.DataFrame:
         # apply function to all word pairs in dataset
-        text1 = df['text_1'].str.strip()
-        text2 = df['text_2'].str.strip()
+        text1 = df[self.text1].str.strip()
+        text2 = df[self.text2].str.strip()
         pairs = pd.concat([text1, text2], axis=1)
-        df['chrfScore'] = pairs.apply(lambda row: chrf_score.sentence_chrf(row.text_1, row.text_2,), axis=1)
+        df['chrfScore'] = pairs.apply(lambda row: chrf_score.sentence_chrf(row[self.text1], row[self.text2]), axis=1)
         return df
