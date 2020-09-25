9726c8b40c978fa8051937d52ef186b62612f0cd
Adam <acohn74@gmail.com>
2020-09-25 13:30:50 +0300

    Fix Bleu

--- src/features/bleu.py
+++ src/features/bleu.py
@@ -8,22 +8,16 @@ from src.features import Metric
 
 class Bleu(Metric):
 
-    def __init__(self):
-        super(Bleu, self).__init__()
-        self.txt_col_format = 'text'
-
-    def setTextFormat(self, val):
-        self.txt_col_format = val
+    def __init__(self, val):
+        super(Bleu, self).__init__(val=val)
 
     def run(self, df: pd.DataFrame) -> pd.DataFrame:
 
-        # for index, row in df.iterrows():
-        #     row[self.txt_col_format+'1'] = row[self.txt_col_format+'1'].strip().split()
-        #     row[self.txt_col_format+'2'] = row[self.txt_col_format+'2'].strip().split()
-        df[f'{self.txt_col_format}1'] = df[f'{self.txt_col_format}1'].str.strip().str.split()
-        df[f'{self.txt_col_format}2'] = df[f'{self.txt_col_format}2'].str.strip().str.split()
-        df['bleu'] = df.apply(lambda row: sentence_bleu([row[f'{self.txt_col_format}1']], row[f'{self.txt_col_format}2']), axis=1) #Open question whether to keep removing of stopwords or not?
-        df['bleu1'] = df.apply(lambda row: sentence_bleu([row[self.txt_col_format+'1']], row[f'{self.txt_col_format}2']), axis=1)
+        text1 = df[self.text1].str.strip().str.split()
+        text2 = df[self.text2].str.strip().str.split()
+        pairs = pd.concat([text1, text2], axis=1)
+        df['bleu'] = pairs.apply(lambda row: sentence_bleu([row[self.text1]], row[self.text2]), axis=1) #Open question whether to keep removing of stopwords or not?
+        df['bleu1'] = pairs.apply(lambda row: sentence_bleu([row[self.text1]], row[self.text2]), axis=1)
 
         return df
 
