f8cf2d9c2a5b794300c55b6656cdf786e2b585c8
Adam <acohn74@gmail.com>
2020-09-25 12:46:34 +0300

    Save progress

--- src/features/bleu.py
+++ src/features/bleu.py
@@ -10,25 +10,18 @@ class Bleu:
     def __init__(self):
         self.txt_col_format = 'text'
 
-    def setTextFormat(self,val):
+    def setTextFormat(self, val):
         self.txt_col_format = val
 
     def run(self, df: pandas.DataFrame) -> pandas.DataFrame:
 
-        for index, row in df.iterrows():
-            row[self.txt_col_format+'1'] = row[self.txt_col_format+'1'].strip().split()
-            row[self.txt_col_format+'2'] = row[self.txt_col_format+'2'].strip().split()
-
-        df['bleu'] = df.apply(lambda row: sentence_bleu([row[self.txt_col_format+'1']], row[self.txt_col_format+'2']), axis=1) #Open question whether to keep removing of stopwords or not?
-        df['bleu1'] = df.apply(lambda row: sentence_bleu([row[self.txt_col_format+'1']], row[self.txt_col_format+'2']), axis=1)
-        # df['bleu_allwords'] = 0
-        # df['bleu_withoutstop'] = 0
-        # for i in range(df.shape[0]):
-        #     s1 = str(df['text_1'][i])
-        #     s2 = str(df['text_2'][i])
-        #     #print(s1,",",s2)
-        #     df.loc[i, 'bleu_allwords'] = self.BLEU1score(s1, s2, stopwords_remove=False)
-        #     df.loc[i, 'bleu_withoutstop'] = self.BLEU1score(s1, s2, stopwords_remove=True)
+        # for index, row in df.iterrows():
+        #     row[self.txt_col_format+'1'] = row[self.txt_col_format+'1'].strip().split()
+        #     row[self.txt_col_format+'2'] = row[self.txt_col_format+'2'].strip().split()
+        df[f'{self.txt_col_format}1'] = df[f'{self.txt_col_format}1'].str.strip().str.split()
+        df[f'{self.txt_col_format}2'] = df[f'{self.txt_col_format}2'].str.strip().str.split()
+        df['bleu'] = df.apply(lambda row: sentence_bleu([row[f'{self.txt_col_format}1']], row[f'{self.txt_col_format}2']), axis=1) #Open question whether to keep removing of stopwords or not?
+        df['bleu1'] = df.apply(lambda row: sentence_bleu([row[self.txt_col_format+'1']], row[f'{self.txt_col_format}2']), axis=1)
 
         return df
 
--- src/run_feature_extract.py
+++ src/run_feature_extract.py
@@ -5,28 +5,34 @@ This file implements running the different semantic similiarity metrics on a dat
 import pickle
 import argparse
 import configparser
-import os.path
+import sys
 from os import path
 
 #import sys
 #print(f"P:{sys.path}")
 
 
-from features.bleu import Bleu
-from features.bertscore import BertScore
-from features.chrFScore import chrFScore
-from features.cosine_similarites import CosineSimilarity
-from features.elmo_euclidean_distance import EuclideanElmoDistance
-from features.ngram_overlap import NgramOverlap
-from features.POS_distance import POSDistance
-from features.ROUGE import ROUGE
-from features.WMD import WMD
+from src.features.bleu import Bleu
+from src.features.bertscore import BertScore
+from src.features.chrFScore import chrFScore
+from src.features.cosine_similarites import CosineSimilarity
+from src.features.elmo_euclidean_distance import EuclideanElmoDistance
+from src.features.ngram_overlap import NgramOverlap
+from src.features.POS_distance import POSDistance
+from src.features.ROUGE import ROUGE
+from src.features.WMD import WMD
 
+
+class config:
+    def __init__(self, pckl: str, features: str, max_n: int):
+        self.pickle = pckl
+        self.features = features
+        self.max_n = max_n
 
 
 def main(args):
 
-    creds_path_ar = ["credentials.ini"]
+    creds_path_ar = ["/Users/adam/PycharmProjects/SOTA/credentials.ini"]
     PATH_ROOT = ""
     PATH_DATA = ""
     GloVe_840B_300d_PATH = ""
@@ -36,12 +42,12 @@ def main(args):
         if path.exists(creds_path):
             config_parser = configparser.ConfigParser()
             config_parser.read(creds_path)
-            PATH_ROOT = config_parser['MAIN']["PATH_ROOT"]
-            PATH_DATA = config_parser['MAIN']["PATH_DATA"]
+            # PATH_ROOT = config_parser['MAIN']["PATH_ROOT"]
+            # PATH_DATA = config_parser['MAIN']["PATH_DATA"]
             GloVe_840B_300d_PATH = config_parser['MAIN']["GloVe_840B_300d_PATH"]
             Glove_twitter_27B_PATH= config_parser['MAIN']["Glove_twitter_27B_PATH"]
-            WANDB_enable = config_parser['MAIN']["WANDB_ENABLE"] == 'TRUE'
-            ENV = config_parser['MAIN']["ENV"]
+            # WANDB_enable = config_parser['MAIN']["WANDB_ENABLE"] == 'TRUE'
+            # ENV = config_parser['MAIN']["ENV"]
             break
 
     picklefile = args.pickle
@@ -57,7 +63,7 @@ def main(args):
         chrf_score=chrFScore(), 
         pos_distance=POSDistance(vector_path=Glove_twitter_27B_PATH), 
         wmd=WMD(vector_path=GloVe_840B_300d_PATH),
-        ngram_overlap=NgramOverlap(args.max_n), 
+        ngram_overlap=NgramOverlap(args.max_n),
         rouge=ROUGE())
                       
     features = args.features
@@ -76,13 +82,17 @@ def main(args):
     with open(picklefile, 'wb') as handle:
         pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pickle', type=str, required=True, help='pickle path for combined dataset')
-    parser.add_argument('--features', required=True, type=str, help='use "ALL" for all features, or comma separated list of features')
-    parser.add_argument('--max_n', type=int, help='maximum number of n-gram overlap score to calculate, e.g. max_n=2 creates 1-gram-overlap & 2-gram-overlap')
-
-    args = parser.parse_args()
-    main(args)
+args = config('/Users/adam/PycharmProjects/SOTA/data/combined/no_annotators/combined_data_no_nans_rerun.pickle', 'bleu', 1)
+main(args)
+# if __name__ == '__main__':
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--pickle', type=str, required=True, default='data/combined/no_annotators/combined_data_no_nans_rerun.pickle',
+#                         help='pickle path for combined dataset')
+#     parser.add_argument('--features', required=True, type=str, default='ALL',
+#                         help='use "ALL" for all features, or comma separated list of features')
+#     parser.add_argument('--max_n', type=int, default=1,
+#                         help='maximum number of n-gram overlap score to calculate, e.g. max_n=2 creates 1-gram-overlap & 2-gram-overlap')
+#
+#     args = parser.parse_args()
+#     main(args)
