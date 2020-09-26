5725d8a059ec0565b64f689bb0041076cef29b05
Adam <acohn74@gmail.com>
2020-09-26 11:20:11 +0300

    Align ngram_overlap.py

--- src/features/ngram_overlap.py
+++ src/features/ngram_overlap.py
@@ -8,7 +8,8 @@ class NgramOverlap(Metric):
         super(NgramOverlap, self).__init__(val=val)
         self.n = n
 
-    def gram_overlap(self, sent_a, sent_b):
+    @staticmethod
+    def gram_overlap(sent_a, sent_b):
         first_sentence_set = set(sent_a)
         second_sentence_set = set(sent_b)
         score_wo = len(first_sentence_set & second_sentence_set) / len(first_sentence_set | second_sentence_set)
@@ -18,5 +19,5 @@ class NgramOverlap(Metric):
         text1 = df['text_1'].str.strip().str.split()
         text2 = df['text_2'].str.strip().str.split()
         pairs = pd.concat([text1, text2], axis=1)
-        df['1-gram_overlap'] = pairs.apply(lambda row: self.gram_overlap(row.text_1, row.text_2,))
+        df['1-gram_overlap'] = pairs.apply(lambda row: self.gram_overlap(row[self.text1], row[self.text2],))
         return df
