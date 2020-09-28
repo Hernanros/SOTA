580d09bcff4a70e58c19fb1dcab8b42e74cc00c2
Adam <acohn74@gmail.com>
2020-09-28 14:42:59 +0300

    Enable handling of csv data files, add error catching and handling during extractor.run()

--- src/run_feature_extract.py
+++ src/run_feature_extract.py
@@ -4,10 +4,12 @@ This file implements running the different semantic similiarity metrics on a dat
 
 import pickle
 import argparse
+import pandas as pd
 import configparser
 import os
 from os import path
 import sys
+from src import Bleu, BertScore, chrFScore, CosineSimilarity, EuclideanElmoDistance, NgramOverlap, POSDistance, ROUGE, WMD
 
 creds_path_ar = [path.join(path.dirname(os.getcwd()), "credentials.ini"), "credentials.ini"]
 PATH_ROOT = ""
@@ -22,28 +24,21 @@ for creds_path in creds_path_ar:
         PATH_ROOT = config_parser['MAIN']["PATH_ROOT"]
         PATH_DATA = config_parser['MAIN']["PATH_DATA"]
         GloVe_840B_300d_PATH = config_parser['MAIN']["GloVe_840B_300d_PATH"]
-        Glove_twitter_27B_PATH= config_parser['MAIN']["Glove_twitter_27B_PATH"]
+        Glove_twitter_27B_PATH = config_parser['MAIN']["Glove_twitter_27B_PATH"]
         WANDB_enable = config_parser['MAIN']["WANDB_ENABLE"] == 'TRUE'
         ENV = config_parser['MAIN']["ENV"]
         break
 
 # adding cwd to path to avoid "No module named src.*" errors
-sys.path.insert(0,os.path.join(PATH_ROOT))
+sys.path.insert(0, os.path.join(PATH_ROOT))
 
-#print(f"P:{sys.path}")
 
-from src.features.bleu import Bleu
-from src.features.bertscore import BertScore
-from src.features.chrFScore import chrFScore
-from src.features.cosine_similarites import CosineSimilarity
-from src.features.elmo_euclidean_distance import EuclideanElmoDistance
-from src.features.ngram_overlap import NgramOverlap
-from src.features.POS_distance import POSDistance
-from src.features.ROUGE import ROUGE
-from src.features.WMD import WMD
+# print(f"P:{sys.path}")
+
 
 class Config:
     """Config class for debugging in IDE"""
+
     def __init__(self, pckl: str, features: str, max_n: int):
         self.pickle = pckl
         self.features = features
@@ -51,25 +46,27 @@ class Config:
 
 
 def main(args):
-
     picklefile = args.pickle
 
-    with open(picklefile, 'rb') as handle:
-        df = pickle.load(handle)
+    if 'pickle' in picklefile:
+        with open(picklefile, 'rb') as handle:
+            df = pickle.load(handle)
+    else:
+        df = pd.read_csv(picklefile, index_col=0)
 
     txt_col_format = 'text_' if 'text_1' in df.columns else 'text'
 
     extractors = dict(
         bleu=Bleu(txt_col_format),
         cosine_similarites=CosineSimilarity(val=txt_col_format, glove_path=Glove_twitter_27B_PATH),
-        elmo_similarites=EuclideanElmoDistance(val=txt_col_format),
+        elmo=EuclideanElmoDistance(val=txt_col_format),
         bert=BertScore(val=txt_col_format),
         chrf_score=chrFScore(val=txt_col_format),
         pos_distance=POSDistance(val=txt_col_format, vector_path=Glove_twitter_27B_PATH),
         wmd=WMD(val=txt_col_format, vector_path=GloVe_840B_300d_PATH),
         ngram_overlap=NgramOverlap(args.max_n, val=txt_col_format),
         rouge=ROUGE(val=txt_col_format))
-                      
+
     features = args.features
     if features == 'ALL':
         features = list(extractors.keys())
@@ -78,23 +75,35 @@ def main(args):
 
     for feature_name, extractor in extractors.items():
         if feature_name in features:
-            df = extractor.run(df)
+            if feature_name not in df.columns:
+                try:
+                    print(f'Running {feature_name} metric')
+                    df = extractor.run(df)
+                except Exception as e:
+                    print(f'Threw error on {feature_name}')
+                    print(f'Threw {type(e)} with message "{e}"')
 
-    with open(picklefile, 'wb') as handle:
-        pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if 'pickle' in picklefile:
+        with open(picklefile, 'wb') as handle:
+            pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        df.to_csv(picklefile, index=False)
 
-# args = Config('/Users/adam/PycharmProjects/SOTA/data/combined/no_annotators/combined_data_no_nans_rerun.pickle', 'elmo_similarites', 1)
-# main(args)
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pickle', type=str, required=True, default='data/combined/no_annotators/combined_data_no_nans_rerun.pickle',
-                        help='pickle path for combined dataset')
-    parser.add_argument('--features', required=True, type=str, default='ALL',
-                        help='use "ALL" for all features, or comma separated list of features')
-    parser.add_argument('--max_n', type=int, default=1,
-                        help='maximum number of n-gram overlap score to calculate, e.g. max_n=2 creates 1-gram-overlap & 2-gram-overlap')
+feats = 'bleu,cosine_similarites,chrf_score,pos_distance,wmd,ngram_overlap,rouge'
+arguments = Config('/Users/adam/PycharmProjects/SOTA/data/combined/with_annotators/combined_dataset.csv', feats, 1)
+main(arguments)
 
-    args = parser.parse_args()
-    main(args)
+# if __name__ == '__main__':
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--pickle', type=str, required=True, default='data/combined/no_annotators/combined_data_no_nans_rerun.pickle',
+#                         help='pickle path for combined dataset')
+#     parser.add_argument('--features', required=True, type=str, default='ALL',
+#                         help='use "ALL" for all features, or comma separated list of features')
+#     parser.add_argument('--max_n', type=int, default=1,
+#                         help='maximum number of n-gram overlap score to calculate, e.g. max_n=2 creates 1-gram-overlap & 2-gram-overlap')
+#
+#     args = parser.parse_args()
+#     main(args)
