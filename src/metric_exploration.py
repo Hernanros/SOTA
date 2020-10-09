40dc945cdb27b06460ba635706c2b3569e03532f
Shaul Solomon <shaulsolomon@gmail.com>
2020-10-09 17:50:05 +0200

    Visualizations Ready

--- src/metric_exploration.py
+++ src/metric_exploration.py
@@ -17,6 +17,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+import plotly.express as px
+from ipywidgets import interact
+
 
 # non_metric_columns = ['text1','text2','label','dataset','random','duration','total_seconds','pair_id','reduced_label','annotator','radical','radical_random','radical_non_random','is_radical','is_centralist','num_labels','bad_annotator']
 
@@ -28,12 +31,14 @@ class Metrics_Corr():
         df -- {pd.DataFrame} -- our combined metrics with all metrics and distinctions
         non_metrics_columns -- {list} -- all of the columns that arent metrics
         categories -- {list} -- list for each type of categories we want to filter by (by default ['dataset','random])
+        include_reduced_label -- {bool} - flag whether or not to include the reduced label
     '''
 
-    def __init__(self,df, non_metric_columns, categories = ['dataset','random']):
+    def __init__(self,df, non_metric_columns, categories = ['dataset','random'], include_reduced_label = True):
         self.df = df
         self.non_metric_columns = non_metric_columns
         self.categories = categories
+        self.include_reduced_label = include_reduced_label
 
     def get_corr(self, bad_annotator: list) -> dict:
         '''
@@ -65,15 +70,19 @@ class Metrics_Corr():
             reduced_label_corr = dict()
             for name,group in df.groupby(category):
                 label_corr[name] = group[metrics].corrwith(group['label'])
-                reduced_label_corr[name] = group[metrics].corrwith(group['reduced_label'])
+                if self.include_reduced_label:
+                    reduced_label_corr[name] = group[metrics].corrwith(group['reduced_label'])
             correlations_dict['label_by_' + category] = pd.DataFrame.from_dict(label_corr).T
-            correlations_dict['reduced_label_by_' + category] = pd.DataFrame.from_dict(reduced_label_corr).T
+            if self.include_reduced_label:
+                correlations_dict['reduced_label_by_' + category] = pd.DataFrame.from_dict(reduced_label_corr).T
 
         combined_datasets_label_corr = df[metrics].corrwith(df['label'])
-        combined_datasets_reduced_label_corr = df[metrics].corrwith(df['reduced_label'])
+        if self.include_reduced_label:
+            combined_datasets_reduced_label_corr = df[metrics].corrwith(df['reduced_label'])
 
         correlations_dict['label_by_combined'] = pd.Series(combined_datasets_label_corr)
-        correlations_dict['reduced_label_by_combined'] = pd.Series(combined_datasets_reduced_label_corr)
+        if self.include_reduced_label:
+            correlations_dict['reduced_label_by_combined'] = pd.Series(combined_datasets_reduced_label_corr)
 
         return correlations_dict
 
@@ -112,18 +121,21 @@ class Metrics_Models():
         df -- {pd.DataFrame} -- our combined metrics with all metrics and distinctions
         non_metric_columns -- {list} -- all of the columns that arent metrics
         categories -- {list} -- list for each type of categories we want to filter by (by default ['dataset','random])
+        include_reduced_label -- {bool} - flag whether or not to include the reduced label
     '''
 
-    def __init__(self,df,non_metric_columns, categories = ['dataset','random']):
+    def __init__(self,df,non_metric_columns, categories = ['dataset','random'], include_reduced_label = True):
         self.non_metric_columns = non_metric_columns
         self.categories = categories
 
         self.metrics = [x for x in df.columns if x not in self.non_metric_columns]
         all_labels = self.metrics + ['label'] + ['reduced_label']
+        self.core_df = df
         self.df = df.groupby(['pair_id'] + self.categories)[all_labels].mean().reset_index()
 
         #Nans dont work for linear and non-linear moels
         self.df.dropna(axis='index', inplace=True)
+        self.include_reduced_label = include_reduced_label
 
 
     def get_data_scaled(self, df):
@@ -191,39 +203,6 @@ class Metrics_Models():
         else:
             return model(X_test).detach().numpy(), model
 
-
-    def run_model(self, model_type = "RF", max_depth = 3, num_hl = 128):
-        '''
-        Multi-Layered Perceptron.
-
-        Parameters:
-            num_hl -- {int} -- size of the hidden layer
-            X_train -- {np.array} -- Train data
-            y_train -- {np.array} - Train label
-            X_test -- {np.array} - Test data
-        
-        Return:
-            y_pred -- {np.array} - Test predicted labels
-            model -- {model} -- The MLP Model
-        '''
-        model = Basemodel(len(self.metrics),num_hl,1)
-        criterion = nn.MSELoss()
-        optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-        X_train = X_train.to_numpy()
-        y_train = y_train.to_numpy()
-        X_test = torch.Tensor(X_test.to_numpy()).to(dtype=torch.float32)
-
-        train_set = DS(X_train,y_train)
-        train_loader=DataLoader(dataset= train_set, batch_size = 32, shuffle = True, num_workers = 2)
-        model = train_epoch(train_loader,model,criterion,optimizer,num_epochs= 30)
-        
-        if torch.cuda.is_available:
-            return model(X_test).cpu().detach().numpy(), model
-        else:
-            return model(X_test).detach().numpy(), model
-
-
     def run_model(self, model_type = "RF", max_depth = 3, num_hl = 128):
         '''
         Run either the RF or the MLP.
@@ -257,19 +236,21 @@ class Metrics_Models():
                 data = self.get_data_scaled(group)
 
                 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
-                X_train_reduced, X_test_reduced, y_train_reduced, y_test_reduced = train_test_split(data, labels_reduced, test_size=0.2)
+                if self.include_reduced_label:
+                    X_train_reduced, X_test_reduced, y_train_reduced, y_test_reduced = train_test_split(data, labels_reduced, test_size=0.2)
 
                 #Get the score from the models
                 y_pred, model = eval("self."+model_type)(max_depth,X_train,y_train,X_test)
 
                 category_scores[model_type + '_label_by_' + str(category)+ "_" + str(name)] = mean_squared_error(y_test, y_pred).T
-
-                y_pred_reduced, model2 = eval("self."+model_type)(max_depth,X_train_reduced,y_train_reduced,X_test_reduced)
-                category_scores[model_type + '_label_reduced_by_' + str(category)+ "_" + str(name)] = mean_squared_error(y_test_reduced, y_pred_reduced).T
+                if self.include_reduced_label:
+                    y_pred_reduced, model2 = eval("self."+model_type)(max_depth,X_train_reduced,y_train_reduced,X_test_reduced)
+                    category_scores[model_type + '_label_reduced_by_' + str(category)+ "_" + str(name)] = mean_squared_error(y_test_reduced, y_pred_reduced).T
 
                 if model_type == "RF":
                     feature_importance['fi_label_by_' + str(category)+ "_" + str(name)]  = pd.DataFrame({'feature': data.columns.values, 'importance':model.feature_importances_}).sort_values('importance', ascending=False) 
-                    feature_importance['fi_reduced_label_by_' + str(category)+ "_" + str(name)]  = pd.DataFrame({'feature': data.columns.values, 'importance':model2.feature_importances_}).sort_values('importance', ascending=False) 
+                    if self.include_reduced_label:
+                        feature_importance['fi_reduced_label_by_' + str(category)+ "_" + str(name)]  = pd.DataFrame({'feature': data.columns.values, 'importance':model2.feature_importances_}).sort_values('importance', ascending=False) 
             
             scores[category] = pd.DataFrame.from_dict(category_scores, orient='index').T
             
@@ -287,12 +268,14 @@ class Metrics_Models():
         y_pred, model = eval("self."+model_type)(max_depth,X_train,y_train,X_test)
         scores[model_type + '_label_combined'] = mean_squared_error(y_test, y_pred).T
 
-        y_pred_reduced, model2 = eval("self."+model_type)(max_depth,X_train_reduced,y_train_reduced,X_test_reduced)
-        scores[model_type + '_label_reduced_combined'] = mean_squared_error(y_test_reduced, y_pred_reduced).T
+        if self.include_reduced_label:
+            y_pred_reduced, model2 = eval("self."+model_type)(max_depth,X_train_reduced,y_train_reduced,X_test_reduced)
+            scores[model_type + '_label_reduced_combined'] = mean_squared_error(y_test_reduced, y_pred_reduced).T
 
         if model_type == "RF":
             fi_values['fi_label_combined']  = pd.DataFrame({'feature': data.columns.values, 'importance':model.feature_importances_}).sort_values('importance', ascending=False) 
-            fi_values['fi_reduced_label_combined']  = pd.DataFrame({'feature': data.columns.values, 'importance':model2.feature_importances_}).sort_values('importance', ascending=False) 
+            if self.include_reduced_label:
+                fi_values['fi_reduced_label_combined']  = pd.DataFrame({'feature': data.columns.values, 'importance':model2.feature_importances_}).sort_values('importance', ascending=False) 
 
 
         if model_type == "RF":
@@ -300,13 +283,27 @@ class Metrics_Models():
 
         else:
             return scores
+            
+    def compare_score(self,bad_annotator : list, model_type : str) -> dict:
+        '''
+        Compare the MSE Error between the base dataset and the filtered dataset.
+        '''
+        df2 = self.core_df[~self.core_df.isin(bad_annotator)]
+        model2 = Metrics_Models(df2, self.non_metric_columns, self.categories, self.include_reduced_label)
+        if model_type == 'RF':
+            score_base, _ = self.run_model(model_type)
+            score_filtered, _ = model2.run_model(model_type)
+        else:
+            score_base = self.run_model(model_type)
+            score_filtered = model2.run_model(model_type)
+
+# { (some_key if condition else default_key):(something_if_true if condition
+#           else something_if_false) for key, value in dict_.items() }
 
 
-
-
-
-
-
+        #presuming the filtered model should be better (lower scores) we take the base - the filtered to see how much improved
+        combined_scores = {key : (score_base[key].sub(score_filtered[key]) if isinstance(val,dict) else score_base[key] - score_filtered[key])  for key,val in score_base.items()}
+        return combined_scores
 
 
 class DS(Dataset):
@@ -370,3 +367,38 @@ def train_epoch(tr_loader,model,criterion,optimizer, num_epochs):
             optimizer.zero_grad()
       
     return model
+
+def visualize_score(scores_dict, title = "MSE Loss"):
+    comb_scores = dict()
+    for key,value in scores_dict.items():
+        if isinstance(value, float):
+            comb_scores[key] = value
+        #If the value is a DataFrame
+        else:
+            for (x,y) in zip(list(scores_dict[key].columns),scores_dict[key].values[0]):
+                comb_scores[x] = y
+    df = pd.DataFrame.from_dict(comb_scores, orient='index').reset_index().reset_index()
+    df.columns = ['index','category','MSE_Loss']
+    fig = px.line(df, x="index", y="MSE_Loss", color="category", title=title, hover_data={'index':False})
+    fig.update_traces(mode="markers")
+    fig.update_layout(height=600, width=1200)
+    fig.show()
+
+def visualize_fi(fi_values, categories):
+    for categ in categories:
+        @interact
+        def plot_fi_dataset(key_v = fi_values[categ].keys()):
+            plt.figure(figsize=(10,5))
+            chart = sns.barplot(list(fi_values[categ][key_v].feature), fi_values[categ][key_v].importance);
+            chart.set_xticklabels(chart.get_xticklabels(),rotation=45);
+            chart.set_title(categ)
+            plt.show();
+
+    others = [x for x in fi_values.keys() if x not in categories]
+    @interact
+    def plot_fi(key_v = others):
+        plt.figure(figsize=(10,5))
+        chart = sns.barplot(list(fi_values[key_v].feature), fi_values[key_v].importance);
+        chart.set_xticklabels(chart.get_xticklabels(),rotation=45);
+        chart.set_title(key_v)
+        plt.show();
