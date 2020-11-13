fcf5f789b792e30bd4c30de9ad1e93ae9a912ecf
Adam Cohn <38065591+adcoh@users.noreply.github.com>
2020-11-13 22:05:59 +0200

    Further customization

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,7 +10,7 @@ def text_preprocessing(text_series: pd.Series) -> pd.Series:
     decoded = text_series.progress_apply(lambda text: unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode())
     tqdm.pandas(desc=f'Preprocessing {text_series.name} - expand contractions')
     uncontracted = decoded.progress_apply(lambda text: contractions.fix(text, leftovers=True, slang=True))
-    print(f'Preprocessing {text_series.name} - removing punctuation')
+    print(f'Preprocessing {text_series.name} - remove punctuation')
     no_punctuation = uncontracted.str.replace(r'[^\w\s]', ' ', regex=True).str.replace(r'\s+', ' ', regex=True).str.strip()
     print(f'Preprocessing {text_series.name} - convert to lowercase')
     lowercase = no_punctuation.str.lower().replace(r'^\s*$', np.nan, regex=True)
