e95dc4336044fb188d53107c4c504e7dcbf07596
Adam Cohn <38065591+adcoh@users.noreply.github.com>
2020-11-13 21:54:14 +0200

    Remove empty string examples, add customized progress bar

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,15 @@
 import unicodedata
 import contractions
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
 
 def text_preprocessing(text_series: pd.Series) -> pd.Series:
-    decoded = text_series.apply(lambda text: unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode())
-    uncontracted = decoded.apply(lambda text: contractions.fix(text, leftovers=True, slang=True))
+    tqdm.pandas(desc=f'Preprocessing {text_series.name}')
+    decoded = text_series.progress_apply(lambda text: unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode())
+    uncontracted = decoded.progress_apply(lambda text: contractions.fix(text, leftovers=True, slang=True))
     no_punctuation = uncontracted.str.replace(r'[^\w\s]', ' ', regex=True).str.replace(r'\s+', ' ', regex=True).str.strip()
-    lowercase = no_punctuation.str.lower()
+    lowercase = no_punctuation.str.lower().replace(r'^\s*$', np.nan, regex=True)
     return lowercase
 
